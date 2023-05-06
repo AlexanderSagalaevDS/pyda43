f3791dcd7ebb376ba72a003419410edca7e96d82
Alexander Sagalaev <132747418+AlexanderSagalaevDS@users.noreply.github.com>
2023-05-06 20:04:55 +0300

    Update datatypes_cycles_2.py

--- datatypes_cycles_2.py
+++ datatypes_cycles_2.py
@@ -1,2 +1,12 @@
 #Домашнее задание к лекции "Введение в типы данных и циклы. Часть 2"
 
+companies_capitaliaion= [
+    ['Orange', 1],
+    ['Microsoft',2],
+    ['Bell',3],
+    ['SpaceX',4]  
+    
+    ]
+
+for company in companies_capitaliaion:
+    print(company[0],company[1])
